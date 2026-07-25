4a901f52f452937f68233f5a0026bd7c8a974ab9
Moksh Desai <desaimoksh2008@gmail.com>
2026-07-25 12:42:31 +0530

    Create substations API

---
+++ backend/app/api/substations.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+
+@router.get("/substations/{region}")
+def get_substations(region: str):
+
+    return {
+        "region": region
+    }
+`
